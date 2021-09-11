9fe8d1dfffc494895a77aafe2fe6eff3de50b512
illia <ilr00743@gmail.com>
2021-09-11 20:43:27 +0300

    Initial commit

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Rotator : MonoBehaviour
 {
+    private event UnityAction Spin;
+    void OnEnable()
+    {
+        Spin += OnSpin;
+    }
     void Update()
     {
-        gameObject.transform.Rotate(0,0, 0.5f);
+        Spin?.Invoke();
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnSpin()
+    {
+        if(Input.anyKey)
+            gameObject.transform.Rotate(0, 0, 1.5f);
     }
 }
