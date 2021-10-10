33c07d027e38118e38607a60239d4b8be8c3b0b2
illia <ilr00743@gmail.com>
2021-10-10 23:27:30 +0300

    Initial commit

--- Assets/Scripts/Wheel/Picker.cs
+++ Assets/Scripts/Wheel/Picker.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Picker : MonoBehaviour
 {
-    private Collider2D _collider;
-
-    private void Awake()
-    {
-        _collider = GetComponent<Collider2D>();
-    }
-
-    public Collider2D GetCollider2D()
-    {
-        return _collider;
-    }
+    
 }
